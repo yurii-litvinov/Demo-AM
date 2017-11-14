83d6fb341a68ab5b4be2b36261d5e8cb4b6a5e6c
Unknown <notavailablenow@mail.ru>
2017-11-14 18:50:28 +0300

    Функция поиска вроде доделана

--- Team_A/Team_A/Structures.cpp
+++ Team_A/Team_A/Structures.cpp
@@ -9,7 +9,17 @@ Node* find(Tree* tree, int value) {
 		if (nodeNow->leftChild != nullptr) {
 			nodeNow = nodeNow->leftChild;
 			presentValue = nodeNow->value;
+			break;
 		}
+		if (nodeNow->rightChild != nullptr) {
+			nodeNow = nodeNow->rightChild;
+			presentValue = nodeNow->value;
+			break;
+		}
+		if (nodeNow->leftChild == nullptr && nodeNow->rightChild == nullptr) {
+			return nullptr;
+		}
+		return nodeNow;
 	}
 }
 
