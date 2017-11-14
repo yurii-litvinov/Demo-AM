0794bf066ef95bfeccbaaafb0841bb2b12e33e0c
Unknown <notavailablenow@mail.ru>
2017-11-14 18:31:44 +0300

    Поиск начат

--- Team_A/Team_A/Structures.cpp
+++ Team_A/Team_A/Structures.cpp
@@ -1,2 +1,13 @@
 #include "Structures.h"
 
+Node* find(Tree* tree, int value) {
+	int presentValue = tree->head->value;
+	Node* finded = nullptr;
+	Node* watchNow = tree->head;
+	while (presentValue != value) {
+		if (watchNow->leftChild != nullptr) {
+
+		}
+	}
+
+}
