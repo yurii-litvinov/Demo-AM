85b771ece26ed72acf0a234b0e82218e08fb68d0
Nikita Rassudov <rassudov99@gmail.com>
2017-11-14 15:35:07 +0300

    Bug fixing

--- Team_A/Team_A/Structures.cpp
+++ Team_A/Team_A/Structures.cpp
@@ -1,6 +1,15 @@
 #include "Structures.h"
+#include <iostream>
 
+Node* find(Tree* tree, int value)
+{
+
+}
+void deleteNode(Tree* tree, int value)
+{
+
+}
 void addNode(Tree* tree, int value)
 {
-	
+
 }
