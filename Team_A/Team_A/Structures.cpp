d9759e1bdeeaad9c945f0df9fe7da8e5636b7c37
Nikita Rassudov <rassudov99@gmail.com>
2017-11-28 18:06:09 +0300

    Прописал функцию добавления элемента в дерево

--- Team_A/Team_A/Structures.cpp
+++ Team_A/Team_A/Structures.cpp
@@ -1,22 +1,27 @@
 #include "Structures.h"
 #include <iostream>
 
-Node* find(Tree* tree, int value) {
+Node* find(Tree* tree, int value)
+{
 	int presentValue = tree->head->value;
 	Node* finded = nullptr;
 	Node* nodeNow = tree->head;
-	while (presentValue != value) {
-		if (nodeNow->leftChild != nullptr) {
+	while (presentValue != value)
+	{
+		if (nodeNow->leftChild != nullptr)
+		{
 			nodeNow = nodeNow->leftChild;
 			presentValue = nodeNow->value;
 			break;
 		}
-		if (nodeNow->rightChild != nullptr) {
+		if (nodeNow->rightChild != nullptr)
+		{
 			nodeNow = nodeNow->rightChild;
 			presentValue = nodeNow->value;
 			break;
 		}
-		if (nodeNow->leftChild == nullptr && nodeNow->rightChild == nullptr) {
+		if (nodeNow->leftChild == nullptr && nodeNow->rightChild == nullptr)
+		{
 			return nullptr;
 		}
 		return nodeNow;
@@ -29,6 +34,37 @@ void deleteNode(Tree* tree, int value)
 }
 void addNode(Tree* tree, int value)
 {
-
-
+	Node* current = tree->head;
+	bool found = false;
+	while (!found)
+	{
+		if (value < current->value)
+		{
+			if(current->leftChild != nullptr)
+				current = current->leftChild;
+			else
+			{
+				Node* toAdd = new Node;
+				toAdd->value = value;
+				current->leftChild = toAdd;
+				found = true;
+			}
+		}
+		else if (value > current->value)
+		{
+			if(current->rightChild != nullptr)
+				current = current->rightChild;
+			else
+			{
+				Node* toAdd = new Node;
+				toAdd->value = value;
+				current->rightChild = toAdd;
+				found = true;
+			}
+		}
+		else
+		{
+			found = true;
+		}
+	}
 }
