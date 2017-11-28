b5fe9d6325ffe24f1ade06e28492407c743b3360
dimamelnik <dim.meln@yandex.ru>
2017-11-28 18:18:19 +0300

    функция удаления и поиск минимального значения у вершины

--- Team_A/Team_A/Structures.cpp
+++ Team_A/Team_A/Structures.cpp
@@ -23,9 +23,91 @@ Node* find(Tree* tree, int value) {
 	}
 }
 
+Node* findParent(Tree* tree, int value)
+{
+	int presentValue = tree->head->value;
+	Node* finded = nullptr;
+	Node* nodeNow = tree->head;
+	Node* parentNode = nullptr;
+	while (presentValue != value) {
+		if (nodeNow->leftChild != nullptr) {
+			parentNode = nodeNow;
+			nodeNow = nodeNow->leftChild;
+			presentValue = nodeNow->value;
+			break;
+		}
+		if (nodeNow->rightChild != nullptr) {
+			parentNode = nodeNow;
+			nodeNow = nodeNow->rightChild;
+			presentValue = nodeNow->value;
+			break;
+		}
+		if (nodeNow->leftChild == nullptr && nodeNow->rightChild == nullptr) {
+			return nullptr;
+		}
+		return parentNode;
+	}
+}
+
+Node* findMin(Tree* tree, Node* node)
+{
+	Node* current = node;
+	while (current->leftChild != nullptr)
+	{
+		current = current->leftChild;
+	}
+	return current;
+}
+
 void deleteNode(Tree* tree, int value)
 {
-
+	Node* nodeToDelete = find(tree, value);
+	Node* parentNode = findParent(tree, nodeToDelete->value);
+	if (nodeToDelete->leftChild == nullptr && nodeToDelete->rightChild == nullptr)
+	{
+		if (parentNode->leftChild->value == nodeToDelete->value)
+		{
+			parentNode->leftChild = nullptr;
+		}
+		if (parentNode->rightChild->value == nodeToDelete->value)
+		{
+			parentNode->rightChild = nullptr;
+		}
+	}
+	if (nodeToDelete->leftChild != nullptr && nodeToDelete->rightChild == nullptr)
+	{
+		if (parentNode->leftChild->value == nodeToDelete->value)
+		{
+			parentNode->leftChild = nodeToDelete->leftChild;
+		}
+		if (parentNode->rightChild->value == nodeToDelete->value)
+		{
+			parentNode->rightChild = nodeToDelete->leftChild;
+		}
+	}
+	if (nodeToDelete->leftChild == nullptr && nodeToDelete->rightChild != nullptr)
+	{
+		if (parentNode->leftChild->value == nodeToDelete->value)
+		{
+			parentNode->leftChild = nodeToDelete->rightChild;
+		}
+		if (parentNode->rightChild->value == nodeToDelete->value)
+		{
+			parentNode->rightChild = nodeToDelete->rightChild;
+		}
+	}
+	if (nodeToDelete->leftChild != nullptr && nodeToDelete->rightChild != nullptr)
+	{
+		if (parentNode->leftChild->value == nodeToDelete->value)
+		{
+			parentNode->leftChild = findMin(tree, nodeToDelete->rightChild);
+		}
+		if (parentNode->rightChild->value == nodeToDelete->value)
+		{
+			parentNode->rightChild = findMin(tree, nodeToDelete->rightChild);
+		}
+	}
+	delete nodeToDelete;
 }
 void addNode(Tree* tree, int value)
 {
